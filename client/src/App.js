7622c2ff72ff7a9484ccda24a55df280b8970ffa
blessedtt <kurt_cabingan@dlsu.edu.ph>
2023-02-09 10:38:00 +0800

    Added initial page layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
+  return(
+    <div className="Container">
+      <div class ="sidebar">Sidebar</div>
+      <div class ="nav">Navbar</div>
+      <div class ="content">Content</div>
     </div>
   );
 }
-
 export default App;
+
