f0129b560d8ebe10f226e6336355e5fa2da6d402
blessedt <kurt_cabingan@dlsu.edu.ph>
2023-02-12 22:32:38 +0800

    Added layout for inventory tracker tables

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,53 @@ import './App.css';
 function App() {
   return(
     <div className="Container">
-      <div class ="sidebar">Sidebar</div>
-      <div class ="nav">Navbar</div>
-      <div class ="content">Content</div>
+      <div class ="sidebar">
+        <span id ="title">inventory tracker</span>
+        <span id = "sidecontent">
+          Hello!
+        </span>
+       
+      </div>
+      <div class ="nav">
+        test
+      </div>
+
+
+      
+      <main class ="content">
+        <div class = "outer">
+          DATE UPDATED
+        </div>
+        <div class="inner">
+          <div class="date">
+            date updated
+          </div>
+          <div class="item">
+            Item No.
+          </div>
+          <div class="brand">
+            brand
+          </div>
+          <div class="product">
+            product
+          </div>
+          <div class="stock">
+            on-hand stock
+          </div>
+          <div class="status">
+            status
+          </div>
+          <div class="sales">
+            sales
+          </div>
+          <div class="revenue">
+            total revenue
+          </div>
+          <div class="ave">
+            ave. value
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
