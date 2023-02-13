cdd776569bffed1fe40fa594b28a5aef8cc49d0d
Bol3x <carloroleda@gmail.com>
2023-02-13 11:50:44 +0800

    Placed Category and Product into class
    For readability purpose - original was immediately unclear where the functions came from, with classes it's much easier to determine

--- server/modules/category.js
+++ server/modules/category.js
@@ -1,49 +1,53 @@
 //global prisma client
 prisma = global.prisma
 
-//this script handles all product category management with the database
+//Category class - Manages all product category-related operations on the database
 
-//queries database to get all categories
-exports.getCategories = async () => {
-	const categories = await prisma.product_category.findMany({})
-	return categories;
+class Category{
+
+	//queries database to get all categories
+	static async getCategories(){
+		const categories = await prisma.product_category.findMany({})
+		return categories;
+	}
+
+	//adds category to database
+	static async addCategory(req,res, next){
+		await prisma.product_category.create({
+			data:{
+				name: req.body.name
+			}
+		}).then((result) =>{
+			console.dir(result, {depth: null})
+			res.status(201).json("successfully added category");
+		})
+		.catch((err) =>{
+			next(err);
+		});
+	};
+
+	//removes category from database
+	/** req.body
+	 * 		name - name of category to remove
+	 */
+	static async removeCategory(req,res){
+		await prisma.product_category.delete({
+		where:{
+				name: req.body.name
+		} 
+		}).then((result) =>{
+			console.log("removed category.");
+			res.status(200).json("successfully removed category");
+		}).catch((err) =>{
+			res.status(400).json("require")
+			next(err);
+		});
+	};
+
+	static editCategory(req, res){
+		//TODO in sprint 2 :)
+	}
+
 }
 
-//adds category to database
-exports.addCategory = async (req,res) => {
-	await prisma.product_category.create({
-		data:{
-			name: req.body.name
-		}
-	}).then((result) =>{
-		console.dir(result, {depth: null})
-		res.status(200);
-		return result;
-	})
-	.catch((err) =>{
-		res.json(err);
-		res.status(400);
-	});
-};
-
-//removes category from database
-/** req.body
- * 		name - name of category to remove
- */
-exports.removeCategory = (req,res) =>{
-	prisma.product_category.delete({
-	   where:{
-			name: req.body.name
-	   } 
-	}).then((result) =>{
-		console.log("removed category.");
-		res.status(200);
-	}).catch((err) =>{
-		console.dir(err);
-		res.status(400);
-	});
-};
-
-exports.editCategory = (req, res) => {
-	//TODO in sprint 2 :)
-}
+module.exports = Category;
