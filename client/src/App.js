6bd845f868d9ed36f43fb5acd1266fb13ed39991
blessedt <kurt_cabingan@dlsu.edu.ph>
2023-02-15 10:49:00 +0800

    inventory tracker table has been implemented to do: delete page, ui icons, popups

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Table } from './components/Table'
 function App() {
   return(
     <div className="Container">
       <div class ="sidebar">
-        <span id ="title">inventory tracker</span>
+        <span id ="title"><h1>inventory tracker</h1></span>
         <span id = "sidecontent">
           Hello!
         </span>
        
       </div>
       <div class ="nav">
-        test
+        <h1>HOME</h1>
       </div>
 
 
       
       <main class ="content">
-        <div class = "outer">
-          DATE UPDATED
-        </div>
-        <div class="inner">
-          <div class="date">
-            date updated
-          </div>
-          <div class="item">
-            Item No.
-          </div>
-          <div class="brand">
-            brand
-          </div>
-          <div class="product">
-            product
-          </div>
-          <div class="stock">
-            on-hand stock
-          </div>
-          <div class="status">
-            status
-          </div>
-          <div class="sales">
-            sales
-          </div>
-          <div class="revenue">
-            total revenue
-          </div>
-          <div class="ave">
-            ave. value
-          </div>
-        </div>
+        <Table />
+        
       </main>
     </div>
   );
---
+++ client/src/components/Table.js
@@ -0,0 +1,60 @@
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS} from './columns'
+import './table_style.css'
+
+export const Table = () => {
+    // data will not be recreated at every render
+    const columns = useMemo(() => COLUMNS, [])
+    const data = useMemo(() => MOCK_DATA, [])
+    // used alongside useTable (react table)
+
+    const tableInstance = useTable({
+        columns,
+        data
+    })
+
+    const {
+        getTableProps, // destructured in <table> tag
+        getTableBodyProps, // <tbody>
+        headerGroups, 
+        rows, 
+        prepareRow,
+    } = tableInstance
+
+    return (
+        <table {...getTableProps()}>
+            <thead>
+                {
+                    headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps}>
+                            {
+                                headerGroup.headers.map( column =>(
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                ))
+                            }
+                        </tr>
+                    ))
+                }
+                
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {
+                    rows.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map( cell =>{
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+}
