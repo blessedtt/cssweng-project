4432a576e5326a7830a3055984707a25ca1efa87
Bol3x <carloroleda@gmail.com>
2023-02-15 13:01:32 +0800

    React Table Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Table } from './table/table';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Table />
     </div>
   );
 }
---
+++ client/src/table/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+    {
+        Header: "ID",
+        accessor: 'id',
+    },
+    {
+        Header: "Name",
+        accessor: "name",
+    },
+    {
+        Header: "Stock",
+        accessor: "stock",
+    },
+    {
+        Header: "Last Updated",
+        accessor: "last_updated",
+    },
+    {
+        Header: "Category",
+        accessor: "category",
+    },
+    {
+        Header: "Brand",
+        accessor: "Brand",
+    },
+]
