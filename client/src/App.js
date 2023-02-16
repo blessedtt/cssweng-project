c44903703c351ad45b4896e3d1776a125a4f254f
Bolex <carloroleda@gmail.com>
2023-02-16 09:21:13 +0800

    Merge branch 'wip' into initial-backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-import { Table } from './table/table';
-
+import { Table } from './components/Table';
+import { IoAddCircleOutline, IoPencil, IoTrashSharp, } from 'react-icons/io5';
+import { IconContext } from 'react-icons';
 function App() {
-  return (
-    <div className="App">
-      <Table />
+  return(
+    <div className="Container">
+      <div class ="sidebar">
+        <span id ="title"><h1>inventory tracker</h1></span>
+        <span id = "sidecontent">
+          Hello!
+        </span>
+       
+      </div>
+    
+      <div class ="nav">
+       
+        <ul>
+
+          <li id = 'first'>
+              HOME
+          </li>
+          <li>
+            <a href=''>
+              <IconContext.Provider
+                value ={{ color: '#FFFFFFFF', size:'44px'}}
+              >
+                <IoAddCircleOutline />
+              </IconContext.Provider>
+             </a>
+           </li>
+           <li>
+            <a href=''>
+              <IconContext.Provider
+                  value ={{ color: '#FFFFFFFF', size:'44px'}}
+              >
+                  <IoPencil />
+                </IconContext.Provider>
+              </a>
+            </li>
+          <a href=''>
+            <li>
+              <IconContext.Provider
+                value ={{ color: '#FFFFFFFF', size:'44px'}}
+              >
+                <IoTrashSharp />
+            </IconContext.Provider>
+            </li>
+          </a>
+        </ul>
+        
+      </div>
+
+
+      
+      <main class ="content">
+        <Table />
+        
+      </main>
     </div>
   );
 }
-
 export default App;
+
---
+++ client/src/components/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+    {
+        Header: 'DATE UPDATED',
+        accessor: 'date_updated'
+    },
+    {
+        Header: 'ITEM NO.',
+        accessor: 'item_no'
+    },
+    {
+        Header: 'BRAND',
+        accessor: 'brand'
+    },
+    {
+        Header: 'PRODUCT',
+        accessor: 'product'
+    },
+    {
+        Header: 'ON-HAND STOCK',
+        accessor: 'stock'
+    },
+    {
+        Header: 'STATUS',
+        accessor: 'status'
+    },
+    {
+        Header: 'SALES',
+        accessor: 'sales'
+    },
+]
